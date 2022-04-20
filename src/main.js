e23a79b85889e78546d1972f425dc47cb723dfcc
Micah Johnson <micahjohnson721@gmail.com>
2022-04-20 07:18:28 -0400

    movement wip

--- src/main.js
+++ src/main.js
@@ -6,6 +6,13 @@
 import * as THREE from "../build/three.module.js";
 import * as Utils from "./utils.js";
 
+let inputs = {
+  left: false,
+  right: false,
+  forward: false,
+  backward: false,
+};
+
 /** @type {HTMLCanvasElement} */
 let canvas;
 
@@ -26,13 +33,18 @@ render();
  */
 function init() {
   canvas = document.querySelector("#c");
+  canvas.onclick = () => canvas.requestPointerLock();
 
   camera = new THREE.PerspectiveCamera(80, 16 / 9, 1, 10000);
+  camera.position.y = 20;
 
   scene = new THREE.Scene();
   scene.background = new THREE.Color(0x87ceeb);
 
-  renderer = new THREE.WebGLRenderer({ canvas });
+  const grid = new THREE.GridHelper(1000, 100);
+  scene.add(grid);
+
+  renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
   renderer.setPixelRatio(window.devicePixelRatio);
   renderer.setSize(
     ...Utils.getMaximalBounds(window.innerWidth, window.innerHeight)
@@ -40,6 +52,9 @@ function init() {
 
   // attach event handlers
   window.addEventListener("resize", onresize);
+  window.addEventListener("keydown", onkeydown);
+  window.addEventListener("keyup", onkeyup);
+  document.addEventListener("pointerlockchange", onpointerlockchange);
 }
 
 /**
@@ -50,6 +65,18 @@ function render() {
 }
 
 /**
+ * Processes movement and collisions.
+ */
+function process() {
+
+  const inputVector = new THREE.Vector2(
+    inputs.right - inputs.left,
+    inputs.backward - inputs.forward,
+  ).normalize();
+
+}
+
+/**
  * Handles window resizes. Ensures the canvas maintains
  * a 16:9 aspect ratio.
  */
@@ -57,5 +84,74 @@ function onresize() {
   renderer.setSize(
     ...Utils.getMaximalBounds(window.innerWidth, window.innerHeight)
   );
+  render();
+}
+
+/**
+ * Handles keydown events.
+ * @param {KeyboardEvent} e event
+ */
+function onkeydown(e) {
+  switch (e.key) {
+    case "w":
+      inputs.forward = true;
+      break;
+    case "s":
+      inputs.backward = true;
+      break;
+    case "a":
+      inputs.left = true;
+      break;
+    case "d":
+      inputs.right = true;
+      break;
+  }
+}
+
+/**
+ * Handles keyup events.
+ * @param {KeyboardEvent} e event
+ */
+function onkeyup(e) {
+  switch (e.key) {
+    case "w":
+      inputs.forward = false;
+      break;
+    case "s":
+      inputs.backward = false;
+      break;
+    case "a":
+      inputs.left = false;
+      break;
+    case "d":
+      inputs.right = false;
+      break;
+  }
+}
+
+/**
+ * Determines whether the game should recieve mouse input.
+ */
+function onpointerlockchange() {
+  if (document.pointerLockElement === canvas)
+    document.addEventListener("mousemove", onmousemove);
+  else document.removeEventListener("mousemove", onmousemove);
+}
+
+/**
+ * Rotates the camera based on mouse movement.
+ * @param {MouseEvent} e event
+ */
+function onmousemove(e) {
+
+  const euler = new THREE.Euler(0, 0, 0, "YXZ");
+  euler.setFromQuaternion(camera.quaternion);
+  
+  euler.x -= e.movementY * Math.PI / 500;
+  euler.y -= e.movementX * Math.PI / 500;
+
+  euler.x = THREE.MathUtils.clamp(euler.x, -Math.PI / 2, Math.PI / 2);
+  camera.quaternion.setFromEuler(euler);
+
   render();
 }
